f759b6543639806a1ad966da3913dce229b4787c
mittalsanyam <mittalsanyam2011@gmail.com>
2025-01-02 19:37:04 +0530

    new project

---
+++ ninja-reflex/src/components/Box.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="container p-5 mt-5" id="main">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Box',
+    data(){
+        return {
+            boxX:0,
+            boxY:0,
+            box:null,
+            timeout:null,
+            timer:1000,
+            level:0,
+        }
+    },
+    methods:{
+        emitScore(){
+            this.$emit('scoreEvent')
+            this.level+=1
+            this.deleteBox()
+        },
+        createBox(){
+            this.box = document.createElement("div")
+            this.boxX = Math.floor(Math.random()*50)
+            this.boxY = Math.floor(Math.random()*10)
+            this.box.className = "box"
+            this.box.id = "box"
+            this.box.style.left = this.boxX+"pc"
+            this.box.style.top = this.boxY+"pc"
+            this.box.style.position = "relative"
+            this.box.addEventListener("click",this.emitScore)
+            document.getElementById("main").appendChild(this.box)
+            this.$emit('newBox')
+            if (this.level==10){
+                this.level=10
+                this.timer=this.timer-50
+            }
+            this.timeout=setTimeout(
+                ()=>{
+                    this.deleteBox()
+                }
+            , this.timer);
+        },
+        deleteBox(){
+            clearTimeout(this.timeout)
+            if (this.box!=null){
+                this.box.remove()
+            }
+            this.createBox()
+        }
+    },
+    mounted(){
+        this.createBox()
+    },
+    beforeUnmount(){
+        clearTimeout(this.timeout)
+    }
+
+
+}
+</script>
+
+<style>
+.box {
+    height: 5pc;
+    width: 5pc;
+    border-style: solid;
+    border-color: green;
+    border-width: 0.05pc;
+    border-radius: 2.5pc;
+    text-align: center;
+    background-color: aquamarine;
+}
+</style>
---
+++ ninja-reflex/src/views/HomeView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container p-5">
+    <div class="container p-2">
+      <h1>Ninja Reflex</h1>
+    </div>
+    <div class="container p-2">
+      <button v-if="!start" @click="startGame" class="btn btn-primary">Start Game</button>
+      <button v-if="start" @click="endGame" class="btn btn-danger">End Game</button>
+    </div>
+    <div v-if='start'>
+      Total = {{ total }} Hits = {{ hits }} Miss = {{ miss }}
+    </div>
+    <Box v-if="start" @scoreEvent="increaseScore" @newBox="increaseTotal"></Box>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Box from "../components/Box.vue"
+
+export default {
+  name: 'HomeView',
+  components: {
+    Box
+  },
+  data(){
+    return {
+      start:false,
+      hits:0,
+      miss:0,
+      total:0
+    }
+  },
+  methods:{
+    startGame(){
+      this.start=true
+      this.hits=0
+      this.total=0
+      this.miss=0
+    },
+    endGame(){
+      this.start=false
+    },
+    increaseScore(){
+      this.hits+=1
+    },
+    increaseTotal(){
+      this.total+=1
+      this.miss=this.total-this.hits
+    }
+  },
+}
+</script>
